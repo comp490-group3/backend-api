3d2617d973d301e379a6f86e6427368720df8fe8
Mark Kaplan <mak28530@users.noreply.github.com>
2015-09-17 11:49:35 -0700

    PEP8 compliance

--- punchd/serializers.py
+++ punchd/serializers.py
@@ -2,10 +2,12 @@ from django.contrib.auth.models import User
 from .models import Business, Offer, OfferInstance, Punch
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
+
 
 class BusinessSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
