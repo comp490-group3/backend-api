ecb7aea1c374a42125e443e80818d7c1cf1ae3f3
Mark Kaplan <mak28530@users.noreply.github.com>
2015-09-24 10:53:26 -0700

    Fix URL config

--- punchd/urls.py
+++ punchd/urls.py
@@ -8,12 +8,13 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'businesses', views.BusinessViewSet)
 
-router.register(r'offers', views.OfferViewSet)
+router.register(r'offers', views.OfferViewSet, base_name='offer')
 # router.register(r'offerinstances', views.OfferInstanceViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
