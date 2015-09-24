b531802715b345e53516db648cdf100a5c5109a6
Mark Kaplan <mak28530@users.noreply.github.com>
2015-09-24 12:00:53 -0700

    Remove GIS import

--- punchd/migrations/0004_auto_20150923_1520.py
+++ punchd/migrations/0004_auto_20150923_1520.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import django.contrib.gis.db.models.fields
+# import django.contrib.gis.db.models.fields
 
 
 class Migration(migrations.Migration):
