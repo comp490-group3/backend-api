31adc06d1916dd3bb567c4eda6ebcb0c070b599c
Mark Kaplan <mak28530@users.noreply.github.com>
2015-09-29 09:21:10 -0700

    Temporarily disable permissions checks to simplify integration

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -135,9 +135,9 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ],
 
     # 'DEFAULT_RENDERER_CLASSES': [
     #     'rest_framework.renderers.JSONRenderer',
